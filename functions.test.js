7b374b803fc897a9f821fd7cc5d9e9efee5670f0
Wouter Voorbrood <1726514+Breadpaw@users.noreply.github.com>
2023-10-24 21:26:22 +0200

    update test with decypher

--- functions.test.js
+++ functions.test.js
@@ -1,38 +1,45 @@
-import {capitalize, reverseString, calculator, Cypher} from './functions'
+import { capitalize, reverseString, calculator, Cypher } from './functions';
 
 // Capitalize String
 
 test('String: Capitalize first letter', () => {
-    expect(capitalize('laurens')).toMatch('Laurens');
+	expect(capitalize('laurens')).toMatch('Laurens');
 });
 
 // Reverse String
 
 test('String: Reverse string', () => {
-    expect(reverseString('laurens')).toMatch('snerual');
+	expect(reverseString('laurens')).toMatch('snerual');
 });
 
 // Calculator
 
 test('Calculator: add', () => {
-    expect(calculator.add(2,3)).toEqual(5);
-})
+	expect(calculator.add(2, 3)).toEqual(5);
+});
 
 test('Calculator: subtract', () => {
-    expect(calculator.subtract(10,3)).toEqual(7);
-})
+	expect(calculator.subtract(10, 3)).toEqual(7);
+});
 
 test('Calculator: multiply', () => {
-    expect(calculator.multiply(3,7)).toEqual(21);
-})
+	expect(calculator.multiply(3, 7)).toEqual(21);
+});
 
 test('Calculator: divide', () => {
-    expect(calculator.divide(6,4)).toBeCloseTo(1.5);
-})
+	expect(calculator.divide(6, 4)).toBeCloseTo(1.5);
+});
 
 // Cypher
 
 test('Cypher: Encypher', () => {
-    const c = new Cypher();
-    expect(c.encypher('abcz',3)).toMatch('defc');
-})
+	const c = new Cypher();
+	expect(c.encypher('attack at dawn', 5)).toMatch('fyyfhp fy ifbs');
+});
+
+test('Cypher: Decypher', () => {
+	const c = new Cypher();
+	expect(c.decypher('mn pnrc rb pnvxutnw', 9)).toMatch('de geit is gemolken');
+});
+
+test
