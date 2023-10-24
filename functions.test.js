0cec0358ad7ed54b55110ac6936b1f2e1e7b138e
Wouter Voorbrood <1726514+Breadpaw@users.noreply.github.com>
2023-10-24 14:29:56 +0200

    add tests

--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,31 @@
-import {capitalize, reverseString} from './functions'
+import {capitalize, reverseString, calculator} from './functions'
 
-test('Capitalizes word passed in function', () => {
+// Capitalize String
+
+test('String: Capitalize first letter', () => {
     expect(capitalize('laurens')).toMatch('Laurens');
 });
 
-test('Reverse string passed in function', () => {
+// Reverse String
+
+test('String: Reverse string', () => {
     expect(reverseString('laurens')).toMatch('snerual');
 });
+
+// Calculator
+
+test('Calculator: add function', () => {
+    expect(calculator.add(2,3)).toEqual(5);
+})
+
+test('Calculator: subtract function', () => {
+    expect(calculator.subtract(10,3)).toEqual(7);
+})
+
+test('Calculator: multiply function', () => {
+    expect(calculator.multiply(3,7)).toEqual(21);
+})
+
+test('Calculator: divide function', () => {
+    expect(calculator.divide(6,4)).toBeCloseTo(1.5);
+})
