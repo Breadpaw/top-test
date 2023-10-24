f8d5b8652bfa64f9057f39ea5f15eed2047a7abe
Wouter Voorbrood <1726514+Breadpaw@users.noreply.github.com>
2023-10-24 13:43:59 +0200

    add reverse test

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,9 @@
-import {capitalize} from './functions'
+import {capitalize, reverseString} from './functions'
 
 test('Capitalizes word passed in function', () => {
     expect(capitalize('laurens')).toMatch('Laurens');
+});
+
+test('Reverse string passed in function', () => {
+    expect(reverseString('laurens')).toMatch('snerual');
 });
