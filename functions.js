5853f19a895baa6418214f8d530e43d96a1076f5
Wouter Voorbrood <1726514+Breadpaw@users.noreply.github.com>
2023-10-24 16:38:45 +0200

    add code for cypher

--- functions.js
+++ functions.js
@@ -43,12 +43,59 @@ const calculator = {
 	},
 };
 
-const cypher = {
-	alphabet: 'abcdefghijklmnopqrstuvwxyz',
-};
-// module.exports = capitalize;
 
-export { capitalize, reverseString, calculator };
+class Cypher {
+	constructor(){
+		this.alphabetArray = stringToArray('abcdefghijklmnopqrstuvwxyz'); // store alphabet array for reference
+	}
+
+	// Public Functions
+
+	encypher(string, shift) {
+		
+		this.string = string;
+		this.shift = shift;
+
+		let newString = this.#constructNewString();
+
+		return newString;
+	}
+
+	decypher(string, shift) {
+		
+		// return
+	}
+
+	// Private Functions
+
+	#lookUpShiftedSymbol(symbol){
+		if(!symbol.match(/[a-z]/i)){
+			return symbol;
+		} else {
+			const index = this.alphabetArray.indexOf(symbol)
+			let shiftedIndex = index + this.shift
+
+			// in case index surpasses alphabet length, start at beginning
+			if(shiftedIndex > 26) {
+				shiftedIndex -= 26;
+			} else if (shiftedIndex < 0) {
+				shiftedIndex += 26;
+			}
+			return this.alphabetArray[shiftedIndex];
+		}
+	}
+
+	#constructNewString(){
+		const originalStringArray = stringToArray(this.string)
+		let encypheredStringArray = [];
+
+		for (let i = 0; i < this.string.length; i++) {
+			encypheredStringArray.push(this.#lookUpShiftedSymbol(this.string[i]))
+		}
+
+		return encypheredStringArray.join('');
+	}
+}
 
 function stringToArray(string) {
 	let stringArray = [];
